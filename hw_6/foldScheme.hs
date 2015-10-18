715427cde7bee6b01a50e37a5f6230f9eb31631c
vlad24 <vlad.pavlov24@gmail.com>
2015-10-18 15:03:11 +0300

    count elements in foldScheme fixed

--- hw_6/foldScheme.hs
+++ hw_6/foldScheme.hs
@@ -13,11 +13,12 @@ foldScheme f acc connection@(Parallel par1 par2) =    let
                                                           result2  = foldScheme f acc par2
                                                        in f connection result1 result2
 ------------------------------------------------------
-countElementsF   _  x y = x + y
+countElementsF (Element r) _ _ = 1
+countElementsF      _      x y = x + y
 ------------------------------------------------------
 totalResistanceF (Element    r    ) x y = r
 totalResistanceF (Sequential s1 s2) x y = x + y
 totalResistanceF (Parallel   p1 p2) x y = (x * y) / (x + y)
 ------------------------------------------------------
 test1 = foldScheme countElementsF   0 (Parallel (Element 2) (Sequential (Element 1) (Element 1)))
-test2 = foldScheme totalResistanceF 0 (Parallel (Element 2) (Sequential (Element 1) (Element 1)))
+test2 = foldScheme totalResistanceF 0 (Parallel (Element 2) (Sequential (Element 2) (Element 1)))
